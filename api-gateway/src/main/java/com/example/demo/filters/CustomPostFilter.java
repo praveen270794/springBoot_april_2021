4372c47f4d9776b9f8de16d189b654d18f5c5b66
praveen gupta <praveen270794@gmail.com>
2021-04-24 13:02:05 +0530

    update

--- api-gateway/src/main/java/com/example/demo/filters/CustomPostFilter.java
+++ api-gateway/src/main/java/com/example/demo/filters/CustomPostFilter.java
@@ -2,17 +2,40 @@ package com.example.demo.filters;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 
+
+import reactor.core.publisher.Mono;
+
+@Component
 public class CustomPostFilter extends AbstractGatewayFilterFactory<CustomPostFilter.Config> {
 
 public static class Config{
 		
 	}
 
+
+public CustomPostFilter() {
+	super(Config.class);
+
+}
+
+
 @Override
 public GatewayFilter apply(Config config) {
-	// TODO Auto-generated method stub
-	return null;
+
+	return (exchange,chain) ->{
+
+		return chain.filter(exchange).then(Mono.fromRunnable(()->{
+
+			 ServerHttpResponse resp = exchange.getResponse();
+
+			  System.out.println("Response Status Code:=" +resp.getStatusCode());
+
+			System.out.println("Inside Post Filter");
+		}));
+	};
 }
 
 }
--- api-gateway/src/main/java/com/example/demo/filters/CustomPreFilter.java
+++ api-gateway/src/main/java/com/example/demo/filters/CustomPreFilter.java
@@ -6,24 +6,32 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomPreFilter extends AbstractGatewayFilterFactory<CustomPreFilter.Config> {
 
 	public static class Config{
 		
 	}
 
+	public CustomPreFilter() {
+		super(Config.class);
+	}
+
 	@Override
 	public GatewayFilter apply(Config config) {
 
 		
+
 		 return (exchange, chain) -> {
 	            ServerHttpRequest request = exchange.getRequest();
 
-	    URI requestUrl = exchange.getRequiredAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+	 //  URI requestUri = exchange.get
+
 
 	            System.out.println("PRE FILTER =:"+request);
-	           System.out.println("PRE Filter =:"+ requestUrl);
+	         // System.out.println("PRE Filter =:"+ requestUrl);
 	            
 	            return chain.filter(exchange.mutate().build());
 	        };
